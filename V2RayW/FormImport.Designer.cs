131846bbeba0093fdbff60239eda683c7f185e0f
Cenmrev <Cenmrev@users.noreply.github.com>
2018-05-27 18:49:16 +0800

    import customized config files

--- V2RayW/FormImport.Designer.cs
+++ V2RayW/FormImport.Designer.cs
@@ -28,41 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.listBoxCusConfig = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
             this.buttonFinish = new System.Windows.Forms.Button();
+            this.dialog = new System.Windows.Forms.OpenFileDialog();
+            this.labelIndicator = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // listBox1
+            // listBoxCusConfig
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 17;
-            this.listBox1.Location = new System.Drawing.Point(12, 13);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(500, 157);
-            this.listBox1.TabIndex = 0;
+            this.listBoxCusConfig.FormattingEnabled = true;
+            this.listBoxCusConfig.ItemHeight = 17;
+            this.listBoxCusConfig.Location = new System.Drawing.Point(12, 13);
+            this.listBoxCusConfig.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBoxCusConfig.Name = "listBoxCusConfig";
+            this.listBoxCusConfig.Size = new System.Drawing.Size(500, 157);
+            this.listBoxCusConfig.TabIndex = 0;
+            this.listBoxCusConfig.SelectedValueChanged += new System.EventHandler(this.listBoxCusConfig_SelectedValueChanged);
             // 
-            // button1
+            // buttonAdd
             // 
-            this.button1.Location = new System.Drawing.Point(12, 177);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(59, 27);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonAdd.Location = new System.Drawing.Point(12, 177);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(59, 27);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
             // 
-            // button2
+            // buttonRemove
             // 
-            this.button2.Location = new System.Drawing.Point(77, 177);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 27);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Remove";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonRemove.Location = new System.Drawing.Point(77, 177);
+            this.buttonRemove.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(87, 27);
+            this.buttonRemove.TabIndex = 2;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
             // 
             // buttonFinish
             // 
@@ -70,9 +75,25 @@
             this.buttonFinish.Name = "buttonFinish";
             this.buttonFinish.Size = new System.Drawing.Size(75, 27);
             this.buttonFinish.TabIndex = 3;
-            this.buttonFinish.Text = "Finish";
+            this.buttonFinish.Text = "Save";
             this.buttonFinish.UseVisualStyleBackColor = true;
             this.buttonFinish.Click += new System.EventHandler(this.buttonFinish_Click);
+            // 
+            // dialog
+            // 
+            this.dialog.FileName = "config.json";
+            this.dialog.Filter = "Json|*.json";
+            this.dialog.RestoreDirectory = true;
+            this.dialog.Title = "Choose config.json";
+            // 
+            // labelIndicator
+            // 
+            this.labelIndicator.AutoSize = true;
+            this.labelIndicator.Location = new System.Drawing.Point(332, 182);
+            this.labelIndicator.Name = "labelIndicator";
+            this.labelIndicator.Size = new System.Drawing.Size(43, 17);
+            this.labelIndicator.TabIndex = 4;
+            this.labelIndicator.Text = "label1";
             // 
             // FormImport
             // 
@@ -80,10 +101,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(524, 213);
             this.ControlBox = false;
+            this.Controls.Add(this.labelIndicator);
             this.Controls.Add(this.buttonFinish);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBoxCusConfig);
             this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
@@ -91,15 +113,19 @@
             this.Name = "FormImport";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Import customized config.json";
+            this.Load += new System.EventHandler(this.FormImport_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBoxCusConfig;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
         private System.Windows.Forms.Button buttonFinish;
+        private System.Windows.Forms.OpenFileDialog dialog;
+        private System.Windows.Forms.Label labelIndicator;
     }
 }
--- V2RayW/FormImport.cs
+++ V2RayW/FormImport.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace V2RayW
 {
     public partial class FormImport : Form
     {
+        public static List<String> configJsonPaths = new List<string>();
+
         public FormImport()
         {
             InitializeComponent();
@@ -19,7 +22,84 @@ namespace V2RayW
 
         private void buttonFinish_Click(object sender, EventArgs e)
         {
+            labelIndicator.Text = "verifying...";
+            foreach(var jsonpath in configJsonPaths)
+            {
+                try
+                {
+                    ProcessStartInfo info = new ProcessStartInfo();
+                    string v2rayBin = AppDomain.CurrentDomain.BaseDirectory + "v2ray.exe";
+                    info.FileName = v2rayBin;
+                    info.Arguments = "-test -config " + jsonpath;
+                    info.WindowStyle = ProcessWindowStyle.Hidden;
+                    Process proBach = Process.Start(info);
+                    proBach.WaitForExit();
+                    int returnValue = proBach.ExitCode;
+                    if(returnValue != 0)
+                    {
+                        MessageBox.Show(jsonpath + " is not a valid v2ray config file!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        labelIndicator.Text = "";
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    labelIndicator.Text = "";
+                    return;
+                }
+            }
+            Properties.Settings.Default["cusProfiles"] = string.Join("*", configJsonPaths);
             this.Close();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                configJsonPaths.Add(dialog.FileName);
+                listBoxCusConfig.Items.Clear();
+                foreach (var path in configJsonPaths)
+                {
+                    listBoxCusConfig.Items.Add(path);
+                }
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (listBoxCusConfig.SelectedIndex > configJsonPaths.Count || listBoxCusConfig.SelectedIndex < 0)
+            {
+                return;
+            }
+            configJsonPaths.RemoveAt(listBoxCusConfig.SelectedIndex);
+            listBoxCusConfig.Items.Clear();
+            foreach (var path in configJsonPaths)
+            {
+                listBoxCusConfig.Items.Add(path);
+            }
+            listBoxCusConfig_SelectedValueChanged(sender, e);
+        }
+
+        private void FormImport_Load(object sender, EventArgs e)
+        {
+            labelIndicator.Text = "";
+            string[] savedProfiles = Properties.Settings.Default["cusProfiles"].ToString().Split('*');
+            configJsonPaths.Clear();
+            foreach(var p in savedProfiles)
+            {
+                if(p.Trim().Length > 0)
+                {
+                    configJsonPaths.Add(p);
+                    listBoxCusConfig.Items.Add(p);
+                }
+            }
+            listBoxCusConfig_SelectedValueChanged(sender, e);
+        }
+
+        private void listBoxCusConfig_SelectedValueChanged(object sender, EventArgs e)
+        {
+            buttonRemove.Enabled = listBoxCusConfig.SelectedIndex < configJsonPaths.Count && listBoxCusConfig.SelectedIndex >= 0;
         }
     }
 }
